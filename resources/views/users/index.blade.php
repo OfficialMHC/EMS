c000b289c931b972f8931e72596bf6bb15d1d454
OfficialMHC <official.dev.mhc@gmail.com>
2021-07-01 16:16:17 +0600

    User Delete Successfully! Also Customize some Css

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -22,45 +22,43 @@
         </div>
         <div class="row justify-content-center mb-4">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <a href="{{ route('users.create') }}" class="btn btn-primary">
-                            <i class="fa fa-plus"></i>
-                        </a>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="mb-0 table table-striped table-bordered table-hover">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Username</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Email</th>
-                                    <th>Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($users as $user)
-                                <tr>
-                                    <th scope="row">{{ $user->id }}</th>
-                                    <td>{{ $user->username }}</td>
-                                    <td>{{ $user->first_name }}</td>
-                                    <td>{{ $user->last_name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <a class="btn btn-sm mr-2 rounded btn-light" href="{{ route('users.edit', $user->id) }}"><i class='fa fa-pencil-alt'></i></a>
-                                            <a class="btn btn-sm rounded btn-light" href=""><i class='fa fa-trash'></i></a>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <a href="{{ route('users.create') }}" class="btn btn-primary mb-2">
+                    <i class="fa fa-plus"></i>
+                </a>
+                <div class="table-responsive">
+                    <table class="mb-0 table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->first_name }}</td>
+                                <td>{{ $user->last_name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <div class="btn-group">
+                                        <a class="btn btn-sm mr-2 rounded btn-light" href="{{ route('users.edit', $user->id) }}"><i class='fa fa-pencil-alt'></i></a>
+                                        <form action="{{ route('users.destroy', $user->id) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-sm rounded btn-light"><i class='fa fa-trash'></i></button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
