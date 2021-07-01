27f2588ac76e4f10929b49bbe7e7970cd28ea89e
OfficialMHC <official.dev.mhc@gmail.com>
2021-07-02 03:13:00 +0600

    Country Update Successfully!.

--- app/Http/Controllers/Backend/CountryController.php
+++ app/Http/Controllers/Backend/CountryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CountryStoreRequest;
+use App\Http\Requests\CountryUpdateRequest;
 use App\Models\Country;
 use Illuminate\Http\Request;
 
@@ -24,5 +25,16 @@ class CountryController extends Controller
     {
         Country::create($request->validated());
         return redirect()->route('countries.index')->with('message', 'Country Created Successfully!');
+    }
+
+    public function edit(Country $country)
+    {
+        return view('countries.edit', compact('country'));
+    }
+
+    public function update(CountryUpdateRequest $request, Country $country)
+    {
+        $country->update($request->validated());
+        return redirect()->route('countries.index')->with('message', 'Country Updated Successfully!');
     }
 }
