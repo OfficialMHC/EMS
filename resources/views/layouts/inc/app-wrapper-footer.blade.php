a4fb3d46b8c2eb7ef64946220593843077416d86
OfficialMHC <official.dev.mhc@gmail.com>
2021-07-01 13:57:08 +0600

    Setup Admin Template.

---
+++ resources/views/layouts/inc/app-wrapper-footer.blade.php
@@ -0,0 +1,12 @@
+<div class="app-wrapper-footer">
+    <div class="app-footer">
+        <div class="app-footer__inner">
+            <div class="app-footer-left">
+                Copyright © {{ date('Y') }} <a href="http://hanif.intels.co/" class="ml-1 mr-1">OfficialMHC</a> All Rights Reserved.
+            </div>
+            <div class="app-footer-right">
+                Developed By: <a href="https://facebook.com/hanif.mhc" class="ml-1 mr-1">MHC</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>@yield('title') | EMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <meta name="msapplication-tap-highlight" content="no">
+    <!-- CSS LINKS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+    @include('layouts.inc.app-header')
+    <div class="app-main">
+        @include('layouts.inc.app-sidebar')
+        <div class="app-main__outer">
+            <div class="app-main__inner">
+                @yield('content')
+            </div>
+            @include('layouts.inc.app-wrapper-footer')
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
